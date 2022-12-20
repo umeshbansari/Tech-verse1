eb7b48155e9835220ec6f48c4efc68f1bec2a389
umeshbansari <umeshbansari22@gmail.com>
2022-12-20 20:57:50 +0500

    changes

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import {Carousel} from "react-responsive-carousel";
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import img1 from "../Assets/p3.jpg";
 import img2 from "../Assets/p4.jpg";
 const Services = () => {
-  return <div className='services'>Services
-  <Carousel>
-  <div>
-  <img src={img1} alt="Item1"/>
-  <p>Full Stack</p>
-  </div>
-  <div>
-  <img src={img2} alt="Item2"/>
-  <p>Autocad Services also </p>
-  </div>
-  </Carousel>
-  </div>
+  return (
+    <div className="services">
+      Services
+      <Carousel>
+        <div>
+          <img src={img1} alt="Item1" />
+          <p>Full Stack</p>
+        </div>
+        <div>
+          <img src={img2} alt="Item2" />
+          <p>Autocad Services also </p>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
 
-}
-
-export default Services
+export default Services;
